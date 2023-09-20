0e91390d3f8e2155d86a3e6170964fdd3ab71751
JoseIgnacio2020 <jose.ignacio.silva2015@gmail.com>
2023-09-20 19:10:40 -0300

    Se suben los archivos y carpetas del proyecto.

---
+++ src/app/components/empleado-list/count-empleados/count-empleados.component.ts
@@ -0,0 +1,27 @@
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-count-empleados',
+  templateUrl: './count-empleados.component.html',
+  styleUrls: ['./count-empleados.component.css']
+})
+export class CountEmpleadosComponent {
+  @Input() todos: number;
+  @Input() masculino: number;
+  @Input() femenino: number;
+  @Output() countRadioButtonChange = new EventEmitter<string>();
+  radioButtonSeleccionado = 'Todos';
+
+  constructor() {
+    this.todos = 0;
+    this.masculino = 0;
+    this.femenino = 0;
+  }
+
+  ngOnInit(): void {
+  }
+
+  radioChange() {
+    this.countRadioButtonChange.emit(this.radioButtonSeleccionado);
+  }
+}
